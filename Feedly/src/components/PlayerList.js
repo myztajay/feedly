5e750e4dc7016f570b20f8c2368058793036845d
myztajay <myztajay@hotmail.com>
2017-04-19 19:25:26 -0400

    testing if git ignores test key file

--- Feedly/src/components/PlayerList.js
+++ Feedly/src/components/PlayerList.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class PlayerList extends Component {
+  componentWillMount(){
+    console.warn("yo")
+  }
   render(){
     return(
       <View>
