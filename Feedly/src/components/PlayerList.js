a5eb7f58e3dd58f7c8324a0f37dd0c25841c6878
myztajay <myztajay@hotmail.com>
2017-04-19 19:37:12 -0400

    Setup api key, setup api call in playerlist

--- Feedly/src/components/PlayerList.js
+++ Feedly/src/components/PlayerList.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-
+import RiotKey from '../../keys'
 class PlayerList extends Component {
   componentWillMount(){
-    console.warn("yo")
+    fetch(`https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/uvux7?api_key=${RiotKey}`, {
+      method: 'get'
+    })
+    .catch(()=> console.warn('didnt get data'))
+    .then(()=> console.warn('got data'))
   }
   render(){
     return(
