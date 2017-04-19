e1cb03f35b566cc34ecd52ac92e797e0868fc80f
myztajay <myztajay@hotmail.com>
2017-04-19 19:17:33 -0400

    Turned Playerlist from a functional to class based component

--- Feedly/src/components/PlayerList.js
+++ Feedly/src/components/PlayerList.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const PlayerList = () => {
-  return(
-    <View>
-      <Text>PlayerList</Text>
-    </View>
-  )
+class PlayerList extends Component {
+  render(){
+    return(
+      <View>
+        <Text>PlayerList</Text>
+      </View>
+    )
+  }
 }
 
 export default PlayerList;
